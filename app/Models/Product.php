d95060f248885caebf842d947cac1a7959e4f3ab
abdul rahman sholeh <focalornet11@gmail.com>
2025-05-20 17:57:55 +0700

    Menambahkan pagination pada halaman produk

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
 
+    use HasFactory;
     protected $guarded = [];
 
     public function category()
